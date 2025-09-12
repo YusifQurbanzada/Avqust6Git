a27026bcf08c9cee8c5b38b4cd526d4a084f0688
YusifQurbanzada <yusifqurbanzade193@gmail.com>
2025-09-12 15:18:14 +0400

    team Bluudud join today~

---
+++ VS-code/yeterLan.js
@@ -0,0 +1,17 @@
+//ツ.1
+function Sans(s) {
+    if (s % 2 === 0) {
+      return "cut";
+    } 
+    else {
+      return "tek";
+    }
+  }
+Sans(3)
+
+//ツ.2
+function Sans2(n, s, c) {
+    return Math.max(n, s, c);
+  }
+
+Sans2(24, 125, 546)
