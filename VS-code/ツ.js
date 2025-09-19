0f751d316b4e1cb9db77f8bcbf5a7bce9c79e848
YusifQurbanzada <yusifqurbanzade193@gmail.com>
2025-09-19 19:20:29 +0400

    Update

--- VS-code/ツ.js
+++ VS-code/ツ.js
@@ -1,6 +1,27 @@
-const sans = Yusif
-const rand0m = 0
-while(rand0m > 5) {
-    console.log(sans)
-    rand0m++
-}
+//1
+console.log("5" + 3)     
+//2
+console.log("5" - 3)     
+//3
+console.log(5 + true)    
+//4
+console.log(5 + null)    
+//5
+console.log("5" * "2")   
+//6
+console.log("abc" * 2)
+//tap2
+let a = "321" 
+let b = 455   
+let c = true   
+let d = "false" 
+let e = false 
+//tap3
+//?
+//tap 4
+console.log(Boolean(""))   
+console.log(Boolean("hello"))   
+console.log(Boolean(0))         
+console.log(Boolean(1))         
+console.log(Boolean(undefined)) 
+console.log(Boolean(null))      
