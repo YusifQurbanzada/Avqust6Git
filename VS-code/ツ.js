c43f907005ffb713497dca2fc99ebeab0f4194e7
YusifQurbanzada <yusifqurbanzade193@gmail.com>
2025-09-23 20:45:51 +0400

    s

---
+++ VS-code/ツ.js
@@ -0,0 +1,10 @@
+let wantabadtime = "Sans"
+console.log(wantabadtime[4]);
+console.log(wantabadtime[3]);
+console.log(wantabadtime[2]);
+console.log(wantabadtime[1]);
+console.log(wantabadtime[0]);
+
+
+
+
