6ab6a274abded05a15044f81c5dcb5889ae61c16
YusifQurbanzada <yusifqurbanzade193@gmail.com>
2025-09-26 17:31:29 +0400

    iLoveYouSo
    i can never let you go
    in the this world
    you're one for me

--- VS-code/ツ.js
+++ VS-code/ツ.js
@@ -1,10 +1,13 @@
-let wantabadtime = "Sans"
-console.log(wantabadtime[4]);
-console.log(wantabadtime[3]);
-console.log(wantabadtime[2]);
-console.log(wantabadtime[1]);
-console.log(wantabadtime[0]);
-
-
-
-
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    if (i === 5) {
+        break;
+    }
+}
+//tap2
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i);
+}
